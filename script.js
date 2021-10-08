dca30b4de9fb98f15485fe28c6d6a2bfa34397fc
limejack <82292253+limejack@users.noreply.github.com>
2021-10-08 11:32:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+function update()
+{
+	var chatLog = JSON.parse(httpGet('/chatlog.json'));
+	var length = parseInt(chatLog["length"]);
+	var out = "";
+	var fromMessage = "";
+	var textBox = document.getElementById('chatLog');
+	for(x = 0; x < length; x++)
+	{
+		fromMessage = chatLog["messages"][x];
+		out += fromMessage["username"]+" : "+fromMessage["message"];
+		out += "<br>";
+	}
+	textBox.innerHTML = out;
+}
+function sayHi(e)
+{
+	event.preventDefault();
+	if(event.keyCode == 13)
+	{
+		document.getElementById("hi").innerHTML = "hi";
+	}
+}
+inputBox = document.getElementById('inputThingie');
+inputBox.addEventListener('keyup',sayHi);
+setInterval(update,1000);
